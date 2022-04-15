d4ed1b392dc96e0b52e591d135d90d0253b7fb28
Xee545 <76605479+Xee545@users.noreply.github.com>
2022-04-14 19:00:15 -0500

    Update vmoverride.lua

--- vmoverride.lua
+++ vmoverride.lua
@@ -5,11 +5,10 @@ local z = -12
 local function override()
 
   client.RemoveConVarProtection( "tf_viewmodels_offset_override" )
-  client.SetConVar( "tf_viewmodels_offset_override" x, y, z )
+  client.SetConVar( "tf_viewmodels_offset_override", x .. " " .. y .. " " .. z )
   
 end
 
+override()
 print("VM Override Loaded!")
 engine.PlaySound("buttons/button3.wav")
-
-callbacks.Register(why)
