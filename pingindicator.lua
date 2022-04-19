f282f6e178212e88008b35bbe70438a8af672285
Xee545 <76605479+Xee545@users.noreply.github.com>
2022-04-19 09:15:50 -0500

    i love lua (sarcasm)

--- pingindicator.lua
+++ pingindicator.lua
@@ -4,7 +4,7 @@ local font = draw.CreateFont("Consolas", 16, 500, FONTFLAG_OUTLINE, FONTFLAG_ADD
 local function PingIndicator()
   
   local InServer = clientstate.GetClientSignonState() -- Check if the player is fully is a game.
-  -- I'm gonna make a alt version of this bullshit that prints the status of each variable when a key is pressed.
+  -- I'm gonna make a alt version of this bullshit that prints/shows the status of each variable.
   
   if InServer == true then 
     
@@ -12,8 +12,9 @@ local function PingIndicator()
     local ping = entities.GetPlayerResources():GetPropDataTableInt("m_iPing")[me:GetIndex()]
     -- I don't know why, I dont want to know why, I shouldn't have to wonder why, but ping returns as zero. There has to be a better way of doing this right???
     
-    if ping > 100 -- Could it be because of this? If it is then that is complete bullshit.
-      
+    if ping > 100 then -- Could it be because of this? If it is then that is complete bullshit.
+                       -- NOTE: My brain caught onto the fact that I didn't add the "then" statement. If this was the fix, I don't deserve to code anymore.
+   
       draw.SetFont(font)
       draw.Color( 250, 255, 0, 255 )
       draw.Text( 10, 500, "⚠ "..ping )
